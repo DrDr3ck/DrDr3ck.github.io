060a7ed6d60b7b322f60b5d59e7a3b5a57be13d1
Stephane <stephane.conreaux@yahoo.fr>
2020-07-01 10:59:29 +0200

    spinning and image

--- SnowFall/sketch.js
+++ SnowFall/sketch.js
@@ -1,19 +1,41 @@
 const snowflakes = [];
+let gravity;
+let img = null;
+let total = 0;
+
+function preload() {
+    img = loadImage("snowflake.png");
+}
 
 function setup() {
     createCanvas(600,600);
 
     for( let i=0; i < 300; i++) {
         snowflakes.push(new SnowFlake());
+        if( snowflakes[i].r > 8 ) {
+            total++;
+        }
     }
 
-    frameRate(30);
+    img.loadPixels();
+    for( let i=0; i < img.width; i++ ) {
+        for( let j=0; j < img.height; j++ ) {
+            const index = i + j * img.width;
+            if( img.pixels[index*4] === 0 ) {
+                img.pixels[index*4+3] = 0;
+            }
+        }
+    }
+    img.updatePixels();
+
+    gravity = createVector(0,0.03);
 }
 
 function draw() {
     background(0);
 
     snowflakes.forEach(snowflake => {
+        snowflake.applyForce(gravity);
         snowflake.update();
         snowflake.show();
     });
--- SnowFall/snowflake.js
+++ SnowFall/snowflake.js
@@ -1,25 +1,53 @@
 function SnowFlake() {
-    this.position = createVector(random(width),random(height));
+    this.position = createVector(random(width),random(-200,height));
     this.vel = createVector(0,0);
     this.acc = createVector(0,0);
     this.dir = (random(1)>0.5) ? 1 : -1;
-    this.r = random(1,8);
+    const r1 = random(4,16);
+    const r2 = random(4,16);
+    this.r = min(r1,r2);
     this.angle = random(TWO_PI);
     this.offset = 0;
+    this.speed = random(100,200);
+
+    this.applyForce = function(force) {
+        let f = force.copy();
+        f.mult(this.r);
+
+        this.acc.add(f);
+    }
 
     this.update = function() {
-        this.position.y += this.r*0.25;
-        this.offset = sin(this.angle) * this.r;
+        this.vel.add(this.acc);
+        this.vel.limit(this.r*0.2);
+
+        if( this.vel.mag() < 1 ) {
+            this.vel.normalize();
+        }
+
+        this.position.add(this.vel);
+        this.acc.mult(0);
 
         if( this.position.y > height + this.r ) {
             this.position.x = random(width);
             this.position.y = random(-100,-10);
         }
+
+        this.angle += this.dir*this.vel.mag() / this.speed;
+
+        this.offset = sin(this.angle) * this.r * 2;
     }
 
     this.show = function() {
         fill(255);
         stroke(255);
-        ellipse(this.position.x+this.offset, this.position.y, this.r*2, this.r*2);
+        push();
+        translate(this.position.x+this.offset, this.position.y);
+        rotate(this.angle);
+        imageMode(CENTER);
+        //ellipse(0, 0, this.r*2, this.r*2);
+        //rect(-this.r,-this.r,this.r*2, this.r*2);
+        image(img, 0, 0, this.r*2, this.r*2);
+        pop();
     }
 }
