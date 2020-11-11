ca2312da8ebe82f49b4fe8945fad8e2f33785843
Stephane <stephane.conreaux@yahoo.fr>
2020-11-11 12:33:35 +0100

    adding menu button

--- Bakhar/main.js
+++ Bakhar/main.js
@@ -6,11 +6,14 @@ const GAME_START_STATE = 1;
 const GAME_PLAY_STATE = 2;
 const GAME_CREDIT_STATE = 3;
 
+let toggleHelp = false;
+
 let curState = GAME_MENU_STATE;
 
 function getData() {
 	const data = {
-		volume: songVolume
+		volume: songVolume,
+		help: toggleHelp
 	};
 	return data;
 }
@@ -38,6 +41,7 @@ function loadData() {
 		}
 	}
 	songVolume = data.volume;
+	toggleHelp = data.toggleHelp;
 }
 
 const userLang = navigator.language || navigator.userLanguage; // "en-US"
@@ -50,10 +54,10 @@ let tileImages = [];
 let atlas = null;
 
 function preload() {
-    // Load sounds
-    // load images
-    tileImages.push(loadImage('./images/tiles01.png'));
-    tileImages.push(loadImage('./images/tiles02.png'));
+	// Load sounds
+	// load images
+	tileImages.push(loadImage('./images/tiles01.png'));
+	tileImages.push(loadImage('./images/tiles02.png'));
 }
 
 function setup() {
@@ -62,122 +66,138 @@ function setup() {
 	canvas = createCanvas(1200, 800);
 	canvas.parent('canvas');
 
-    frameRate(FPS);
-    
-    atlas = new TileAtlas();
+	frameRate(FPS);
+
+	atlas = new TileAtlas();
 }
 
 function nothing() {
-    console.log("does nothing");
+	console.log('does nothing');
 }
 
 function menuClicked() {
-    curState = GAME_MENU_STATE;
-    manager.setUI(menu);
+	curState = GAME_MENU_STATE;
+	manager.setUI(menu);
 }
 
 function startClicked() {
-    curState = GAME_START_STATE;
-    manager.setUI(start);
+	curState = GAME_START_STATE;
+	manager.setUI(start);
 }
 
 function creditClicked() {
-    curState = GAME_CREDIT_STATE;
-    manager.setUI(credit);
+	curState = GAME_CREDIT_STATE;
+	manager.setUI(credit);
 }
 
-const tileMap = new TileMap();;
+const tileMap = new TileMap();
 
 function newClicked() {
-    curState = GAME_PLAY_STATE;
-    manager.setUI([]);
-    tileMap.init(16,10);
+	curState = GAME_PLAY_STATE;
+	manager.setUI(game);
+	tileMap.init(16, 10);
 }
 
 const menu = [
 	new BButton(100, 400, 'START', startClicked),
 	new BButton(100, 500, 'BLOG', nothing),
-	new BButton(100, 600, 'CREDIT',creditClicked)
+	new BButton(100, 600, 'CREDIT', creditClicked)
 ];
 menu[1].enabled = false;
 
 const start = [
-    new BButton(300,400,'NEW', newClicked),
-    new BButton(300,500,'CONTINUE',nothing),
-    new BButton(300,600,'SAVE',nothing),
-    new BFloatingButton(1100,100,"\u2716",menuClicked)
+	new BButton(300, 400, 'NEW', newClicked),
+	new BButton(300, 500, 'CONTINUE', nothing),
+	new BButton(300, 600, 'SAVE', nothing),
+	new BFloatingButton(1100, 100, '\u2716', menuClicked)
 ];
 start[1].enabled = false;
 start[2].enabled = false;
 
-const credit = [
-    new BFloatingButton(1100,100,"\u2716",menuClicked)
-];
+const credit = [ new BFloatingButton(1100, 100, '\u2716', menuClicked) ];
+
+const game = [ new BMenuButton(30, 675, null, nothing) ];
 
 menuClicked();
 
 let lastTime = Date.now();
 
 function drawBackground() {
-    // background
-    stroke(110,160,130);
-    strokeWeight(3);
-    fill(18,42,27);
-    rect(10,10,width-20,height-20,10);
+	// background
+	stroke(110, 160, 130);
+	strokeWeight(3);
+	fill(18, 42, 27);
+	rect(10, 10, width - 20, height - 20, 10);
+}
+
+function drawGameMenu() {
+	stroke(110, 160, 130);
+	strokeWeight(2);
+	fill(18, 52, 27);
+	rect(15, 665, width - 30, height - 680, 15);
 }
 
 function drawMenu() {
-    drawBackground();
+	drawBackground();
 
-    // grey rectangle
-    fill(51);
-    noStroke();
-    rect(180, 80, width - 180 - 180, 580 - 80);
+	// grey rectangle
+	fill(51);
+	noStroke();
+	rect(180, 80, width - 180 - 180, 580 - 80);
 }
 
 function drawCredit() {
-    drawBackground();
+	drawBackground();
 
-    textSize(32);
-    drawText("Work in progress...", 100, 100);
+	textSize(32);
+	drawText('Work in progress...', 100, 100);
 }
 
 function drawLoading() {
-    drawBackground();
+	drawBackground();
 }
 
 function drawGame() {
-    drawBackground();
-    textSize(32);
-    tileMap.render(20,20);
+	drawBackground();
+	drawGameMenu();
+	textSize(32);
+	tileMap.render(20, 20);
 }
 
-function updateGame(elapsedTime) {
-
-}
+function updateGame(elapsedTime) {}
 
 function processInput() {
 	manager.processInput();
 }
 
 function update(elapsedTime) {
-    if( curState === GAME_PLAY_STATE ) {
-        updateGame(elapsedTime);
-    }
+	if (curState === GAME_PLAY_STATE) {
+		updateGame(elapsedTime);
+	}
 }
 
 function render() {
 	if (curState === GAME_MENU_STATE) {
 		drawMenu();
-	} else if( curState === GAME_CREDIT_STATE ) {
-        drawCredit();
-    } else if( curState === GAME_START_STATE ) {
-        drawLoading();
-    } else if( curState === GAME_PLAY_STATE ) {
-        drawGame();
-    }
-    strokeWeight(1);
-	manager.currentUI.forEach(c => {c.draw();});
+	} else if (curState === GAME_CREDIT_STATE) {
+		drawCredit();
+	} else if (curState === GAME_START_STATE) {
+		drawLoading();
+	} else if (curState === GAME_PLAY_STATE) {
+		drawGame();
+	}
+	strokeWeight(1);
+	manager.currentUI.forEach((c) => {
+		c.draw();
+	});
+
+	if (toggleHelp) {
+		push();
+		textSize(10);
+		const txt = `Mouse ${mouseX},${mouseY}`;
+		drawText(txt, 1100, 770);
+		pop();
+	}
 }
 
 function draw() {
@@ -194,5 +214,11 @@ function mouseMoved() {
 }
 
 function mouseClicked() {
-    manager.mouseClicked();
+	manager.mouseClicked();
+}
+
+function keyPressed() {
+	if (key === 'H') {
+		toggleHelp = !toggleHelp;
+	}
 }
--- Bakhar/ui.js
+++ Bakhar/ui.js
@@ -1,43 +1,43 @@
 class UIManager {
 	constructor() {
-        this.components = [];
-        this.currentUI = [];
-    }
-    
-    setUI(components) {
-        this.currentUI.forEach(c => c.visible = false);
-        this.currentUI = components;
-        this.currentUI.forEach(c => c.visible = true);
-    }
+		this.components = [];
+		this.currentUI = [];
+	}
+
+	setUI(components) {
+		this.currentUI.forEach((c) => (c.visible = false));
+		this.currentUI = components;
+		this.currentUI.forEach((c) => (c.visible = true));
+	}
 
 	processInput() {
-        let over = false;
+		let over = false;
 		this.components.forEach((c) => {
-            c.over = c.mouseOver(mouseX, mouseY);
-            over = over || (c.over && c.isClickable());
-        });
-        if( over ) {
-            cursor('pointer');
-        } else {
-            cursor('default');
-        }
-    }
-    
-    mouseClicked() {
-        let overComponent = null;
-        this.components.forEach((c) => {
-            if( !c.visible ) {
-                return;
-            }
-            if( c.over ) {
-                overComponent = c;
-                return;
-            }
-        });
-        if( overComponent && overComponent.enabled) {
-            overComponent.clicked();
-        }
-    }
+			c.over = c.mouseOver(mouseX, mouseY);
+			over = over || (c.over && c.isClickable());
+		});
+		if (over) {
+			cursor('pointer');
+		} else {
+			cursor('default');
+		}
+	}
+
+	mouseClicked() {
+		let overComponent = null;
+		this.components.forEach((c) => {
+			if (!c.visible) {
+				return;
+			}
+			if (c.over) {
+				overComponent = c;
+				return;
+			}
+		});
+		if (overComponent && overComponent.enabled) {
+			overComponent.clicked();
+		}
+	}
 
 	update(elapsedTime) {
 		this.components.forEach((c) => {
@@ -46,13 +46,13 @@ class UIManager {
 	}
 }
 
-function drawText(string, x, y, enabled=true) {
-    if( enabled ) {
-        fill(198,255,244);
-    } else {
-        fill(128,198,128);
-    }
-	text(string, x, y+4);
+function drawText(string, x, y, enabled = true) {
+	if (enabled) {
+		fill(198, 255, 244);
+	} else {
+		fill(128, 198, 128);
+	}
+	text(string, x, y + 4);
 }
 
 class UIComponent {
@@ -60,16 +60,88 @@ class UIComponent {
 		this.x = x;
 		this.y = y;
 		this.w = width;
-        this.h = height;
-        this.enabled = true;
-        this.visible = false;
+		this.h = height;
+		this.over = false;
+		this.enabled = true;
+		this.visible = false;
 		manager.components.push(this);
 	}
 
 	mouseOver(mx, my) {
-        if( !this.enabled || !this.visible ) {
-            return false;
-        }
+		if (!this.enabled || !this.visible) {
+			return false;
+		}
+		if (mx > this.x + this.w) return false;
+		if (mx < this.x) return false;
+		if (my > this.y + this.h) return false;
+		if (my < this.y) return false;
+		return true;
+	}
+
+	update(elapsedTime) {
+		// virtual pure ?
+	}
+
+	clicked() {
+		// virtual pure ?
+	}
+
+	isClickable() {
+		return false;
+	}
+}
+
+class BButton extends UIComponent {
+	constructor(x, y, text, callback) {
+		const textSize = 60;
+		super(x, y, 400, textSize * 1.2);
+		this.text = text;
+		this.textSize = textSize;
+		this.callback = callback;
+	}
+
+	draw() {
+		if (!this.visible) {
+			return;
+		}
+		push();
+		textAlign(CENTER, CENTER);
+		rectMode(CENTER);
+		textSize(this.textSize);
+		let fRadius = 5;
+		let lRadius = 15;
+		let extend = 0;
+		if (this.over) {
+			stroke(188, 255, 219);
+			fRadius = 15;
+			lRadius = 5;
+			strokeWeight(4);
+			extend = 12;
+		} else {
+			stroke(29, 105, 62);
+			strokeWeight(2);
+		}
+		if (this.enabled) {
+			fill(9, 47, 18);
+		} else {
+			fill(47, 67, 47);
+		}
+
+		rect(this.x + this.w / 2, this.y - this.h / 2, this.w, this.h + extend, fRadius, lRadius);
+		if (this.over) {
+			stroke(188, 255, 219);
+			strokeWeight(2);
+		} else {
+			noStroke();
+		}
+		drawText(this.text, this.x + this.w / 2, this.y - this.h / 2, this.enabled);
+		pop();
+	}
+
+	mouseOver(mx, my) {
+		if (!this.enabled || !this.visible) {
+			return false;
+		}
 		if (mx > this.x + this.w) return false;
 		if (mx < this.x) return false;
 		if (my < this.y - this.h) return false;
@@ -77,122 +149,105 @@ class UIComponent {
 		return true;
 	}
 
-	update(elapsedTime) {
-		// virtual pure ?
-    }
-    
-    clicked() {
-        // virtual pure ?
-    }
-
-    isClickable() {
-        return false;
-    }
-}
-
-class BButton extends UIComponent {
-	constructor(x, y, text, callback) {
-        const textSize = 60;
-		super(x, y, 400, textSize*1.2);
-		this.text = text;
-        this.over = false;
-        this.textSize = textSize;
-        this.callback = callback;
+	clicked() {
+		super.clicked();
+		this.callback();
 	}
 
-	draw() {
-        if( !this.visible ) {
-            return;
-        }
-		push();
-		textAlign(CENTER, CENTER);
-		rectMode(CENTER);
-		textSize(this.textSize);
-		let fRadius = 5;
-        let lRadius = 15;
-        let extend = 0;
-		if (this.over) {
-			stroke(29,105,62);
-			fRadius = 15;
-            lRadius = 5;
-            strokeWeight(4);
-            extend = 12;
-		} else {
-            stroke(188,255,219);
-            strokeWeight(2);
-        }
-        if( this.enabled ) {
-            fill(9,47,18);
-        } else {
-            fill(47,67,47);
-        }
-		
-        rect(this.x + this.w / 2, this.y - this.h / 2, this.w, this.h + extend, fRadius, lRadius);
-        if (this.over) {
-            stroke(188,255,219);
-            strokeWeight(2);
-        } else {
-            noStroke();
-        }
-		drawText(this.text, this.x + this.w / 2, this.y - this.h / 2, this.enabled);
-		pop();
-    }
-    
-    clicked() {
-        super.clicked();
-        this.callback();
-    }
-
-    isClickable() {
-        return this.enabled && this.visible;
-    }
+	isClickable() {
+		return this.enabled && this.visible;
+	}
 }
 
 class BFloatingButton extends UIComponent {
 	constructor(x, y, text, callback) {
-        const textSize = 60;
-		super(x, y, textSize*1.2, textSize*1.2);
+		const textSize = 60;
+		super(x, y, textSize * 1.2, textSize * 1.2);
 		this.text = text;
-        this.over = false;
-        this.textSize = textSize;
-        this.callback = callback;
+		this.textSize = textSize;
+		this.callback = callback;
 	}
 
 	draw() {
-        if( !this.visible ) {
-            return;
-        }
+		if (!this.visible) {
+			return;
+		}
 		push();
 		textAlign(CENTER, CENTER);
 		textSize(this.textSize);
-        let extend = 0;
+		let extend = 0;
 		if (this.over) {
-			stroke(29,105,62);
-            strokeWeight(4);
-            extend = 12;
+			stroke(188, 255, 219);
+			strokeWeight(4);
+			extend = 12;
 		} else {
-            stroke(188,255,219);
-            strokeWeight(2);
-        }
-        fill(9,47,18);
+			stroke(29, 105, 62);
+			strokeWeight(2);
+		}
+		fill(9, 47, 18);
 
-        ellipse(this.x + this.w / 2, this.y - this.h / 2, this.w + extend, this.h + extend);
-        if (this.over) {
-            stroke(188,255,219);
-            strokeWeight(2);
-        } else {
-            noStroke();
-        }
+		ellipse(this.x + this.w / 2, this.y - this.h / 2, this.w + extend, this.h + extend);
+		if (this.over) {
+			stroke(188, 255, 219);
+			strokeWeight(2);
+		} else {
+			noStroke();
+		}
 		drawText(this.text, this.x + this.w / 2, this.y - this.h / 2);
 		pop();
-    }
-    
-    clicked() {
-        super.clicked();
-        this.callback();
-    }
+	}
 
-    isClickable() {
-        return this.enabled && this.visible;
-    }
+	mouseOver(mx, my) {
+		if (!this.enabled || !this.visible) {
+			return false;
+		}
+		if (mx > this.x + this.w) return false;
+		if (mx < this.x) return false;
+		if (my < this.y - this.h) return false;
+		if (my > this.y) return false;
+		return true;
+	}
+
+	clicked() {
+		super.clicked();
+		this.callback();
+	}
+
+	isClickable() {
+		return this.enabled && this.visible;
+	}
+}
+
+class BMenuButton extends UIComponent {
+	constructor(x, y, img, callback) {
+		super(x, y, 100, 100);
+		this.img = img;
+		this.callback = callback;
+	}
+
+	draw() {
+		if (!this.visible) {
+			return;
+		}
+		push();
+    	fill(9, 47, 18);
+        if (this.over) {
+			stroke(188, 255, 219);
+			strokeWeight(4);
+		} else {
+			stroke(29, 105, 62);
+			strokeWeight(2);
+		}
+		rect(this.x, this.y, this.w, this.h, 5);
+		pop();
+	}
+
+	clicked() {
+		super.clicked();
+		this.callback();
+	}
+
+	isClickable() {
+		return this.enabled && this.visible;
+	}
 }
