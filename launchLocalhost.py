322515e429978c1ed5eb964ba327ecce607bf79a
Stephane <stephane.conreaux@yahoo.fr>
2020-11-14 08:25:23 +0100

    improve logic

--- launchLocalhost.py
+++ launchLocalhost.py
@@ -1,9 +1,11 @@
 import sys
 import os
 
-if sys.version_info[0] < 3:
+if sys.version_info[0] == 2:
     print("python -m SimpleHTTPServer")
     os.system("python -m SimpleHTTPServer")
-else:
+elif sys.version_info[0] == 3:
     print("python -m http.server")
     os.system("python -m http.server")
+else:
+    print("You need to install python 2 or python 3")
