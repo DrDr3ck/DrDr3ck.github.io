71bbafbc9d2ac0634cbbdcd424779c057f823b3b
Stephane <stephane.conreaux@yahoo.fr>
2020-11-21 11:14:49 +0100

    adding objects

---
+++ Bakhar/object.js
@@ -0,0 +1,15 @@
+class BObject {
+    constructor(x,y, sizeX, sizeY) {
+        this.x = x;
+        this.y = y;
+        this.size = {x: sizeX, y: sizeY};
+    }
+
+    draw() {
+
+    }
+
+    update(elapsedTime) {
+
+    }
+}
