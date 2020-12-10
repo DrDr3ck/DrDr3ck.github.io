2b6662955ed4aa10221df28aaf29ceff00197738
Stephane <stephane.conreaux@yahoo.fr>
2020-12-10 08:24:09 +0100

    fight and sound

--- Hunt/main.js
+++ Hunt/main.js
@@ -8,42 +8,94 @@ const jobManager = new JobManager();
 const GAME_START_STATE = 1;
 const GAME_PLAY_STATE = 2;
 const GAME_OVER_STATE = 3;
+const GAME_NEXT_LEVEL_STATE = 4;
 let curState = GAME_START_STATE;
 
+const playerStart = {X:4, Y:6};
 let lastTime = 0;
 let firstMove = false;
 let firstBlood = false; // kill an animal
 let firstInjury = false; // get injured by an animal or trap
 
+function getRandomIntInclusive(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function startClicked() {
-	food = 100;
 	curState = GAME_PLAY_STATE;
 	startButton.visible = false;
 	uiManager.addLogger('Use <Arrows> keys to move');
 	uiManager.addLogger('Press SPACE to fight');
+	// reset world
+	world.food = 100;
+	world.player.setTileX(playerStart.X);
+	world.player.setTileY(playerStart.Y);
+	world.level = 1;
+}
+
+function musicClicked() {
+	// TODO
+}
+
+function speakerClicked() {
+	speakerButton.checked = !speakerButton.checked;
 }
 
 const startButton = new BButton(200, 200, 'START', startClicked);
+const speakerButton = new BFloatingButton(730 - 10 - 70, 70, '\uD83D\uDD0A', speakerClicked);
+const musicButton = new BFloatingButton(730, 70, '\uD83C\uDFB6', musicClicked);
 
 const tileSize = 60;
 const world = new World(9, 7);
 world.holes.push(7);
 world.holes.push(23);
-const player = new Player(4, 6);
-
-let food = 0;
 
 const spritesheet = new SpriteSheet();
+let moveSound = null;
+let hitSound = null;
+let animalDeathSound = null;
+let nextLevelSound = null;
+
+function playSound(sound) {
+	if (sound && speakerButton.checked) {
+		sound.play();
+	}
+}
 
 function preload() {
 	spritesheet.addSpriteSheet('ground01', loadImage('./ground01.png'), 60, 60);
 	spritesheet.addSpriteSheet('player01', loadImage('./player01.png'), 60, 60);
+	spritesheet.addSpriteSheet('animal01', loadImage('./animal01.png'), 60, 60);
+
+	moveSound = loadSound("./move.ogg");
+	hitSound = loadSound("./hit.wav");
+	hitSound.setVolume(0.125);
+	gameOverSound = loadSound("./game_over.wav");
+	animalDeathSound = loadSound("./animal_death.wav");
+	nextLevelSound = loadSound("./next_level.wav");
 }
 
 function initUI() {
 	startButton.setTextSize(40);
-	const menu = [ startButton ];
+	speakerButton.setTextSize(50);
+	musicButton.setTextSize(50);
+	musicButton.enabled = false;
+	musicButton.checked = false;
+	const menu = [ startButton, speakerButton, musicButton ];
 	uiManager.setUI(menu);
+}
+
+function fight() {
+	// hurt animals that are in front of the player
+	playSound(hitSound);
+	const damage = world.player.getDamage();
+	for( let i=-1; i <= 1; i++) {
+		for( let j=-1; j <= 1; j++) {
+			world.hurt(world.player.tilePosition.X+i, world.player.tilePosition.Y+j, damage);
+		}
+	}
 }
 
 function movePlayer(move) {
@@ -52,28 +104,42 @@ function movePlayer(move) {
 		uiManager.addLogger('Each move costs food');
 	}
 	let canMove = false;
-	if (move === 'UP' && world.isFree(player.tilePosition.X, player.tilePosition.Y - 1)) {
-		player.setTileY(player.tilePosition.Y - 1);
+	if (move === 'UP' && world.isFree(world.player.tilePosition.X, world.player.tilePosition.Y - 1)) {
+		world.player.setTileY(world.player.tilePosition.Y - 1);
 		canMove = true;
 	}
-	if (move === 'DOWN' && world.isFree(player.tilePosition.X, player.tilePosition.Y + 1)) {
-		player.setTileY(player.tilePosition.Y + 1);
+	if (move === 'DOWN' && world.isFree(world.player.tilePosition.X, world.player.tilePosition.Y + 1)) {
+		world.player.setTileY(world.player.tilePosition.Y + 1);
 		canMove = true;
 	}
-	if (move === 'LEFT' && world.isFree(player.tilePosition.X - 1, player.tilePosition.Y)) {
-		player.setTileX(player.tilePosition.X - 1);
+	if (move === 'LEFT' && world.isFree(world.player.tilePosition.X - 1, world.player.tilePosition.Y)) {
+		world.player.setTileX(world.player.tilePosition.X - 1);
 		canMove = true;
 	}
-	if (move === 'RIGHT' && world.isFree(player.tilePosition.X + 1, player.tilePosition.Y)) {
-		player.setTileX(player.tilePosition.X + 1);
+	if (move === 'RIGHT' && world.isFree(world.player.tilePosition.X + 1, world.player.tilePosition.Y)) {
+		world.player.setTileX(world.player.tilePosition.X + 1);
 		canMove = true;
 	}
 	if (canMove) {
-		food = Math.max(0, food - 1);
-		if (food === 0) {
+		world.food = Math.max(0, world.food - 1);
+		playSound(moveSound);
+		world.move();
+		if (world.food === 0) {
 			// game over
 			curState = GAME_OVER_STATE;
+			playSound(gameOverSound);
 			startButton.visible = true;
+		} else {
+			// check if player is on 'next level' tile
+			if( world.player.tilePosition.X === world.nextTileLevel.X && world.player.tilePosition.Y === world.nextTileLevel.Y ) {
+				playSound(nextLevelSound);
+				curState = GAME_NEXT_LEVEL_STATE;
+				uiManager.addLogger("Next level!!!");
+				setTimeout(function() {
+					world.nextLevel();
+					curState = GAME_PLAY_STATE;
+				}, speakerButton.checked ? 2000 : 800);
+			}
 		}
 	}
 }
@@ -90,19 +156,30 @@ function setup() {
 	uiManager.addLogger('Survive!!! Hunt animals!');
 	lastTime = Date.now();
 
-	player.addAnimation('idle', 'player01', [ 0, 1, 2, 3 ], FPS, true);
-	player.addAnimation('up', 'player01', [ 4, 5, 6, 7 ], FPS, true);
+	world.player = new Player(playerStart.X, playerStart.Y);
+	world.animals.push(new Animal('animal01', 2, 2));
 }
 
 function drawGame() {
 	push();
 	translate(50, 50);
 	world.draw();
-	player.draw();
 	pop();
 	textSize(32);
 	fill(255, 128, 0);
-	text(`FOOD: ${food}`, 600, 132);
+	let textY = 130;
+	text(`FOOD: ${world.food}`, 600, textY);
+	textY+=50;
+	if (world.selectedTile) {
+		text(`Tile ${world.selectedTile.X}/${world.selectedTile.Y}`, 600, textY);
+	}
+	textY+=50;
+	text(`LEVEL: ${world.level}`, 600, textY);
+	textY+=50;
+}
+
+function updateGame(elapsedTime) {
+	world.update(elapsedTime);
 }
 
 function draw() {
@@ -110,17 +187,20 @@ function draw() {
 	const elapsedTime = currentTime - lastTime;
 	background(51);
 
-	uiManager.processInput();
+	if (curState === GAME_PLAY_STATE) {
+		world.mouseMoved(mouseX - 50, mouseY - 50);
+	}
 
+	uiManager.processInput();
 	uiManager.update(elapsedTime);
 
 	// draw game
 	if (curState === GAME_PLAY_STATE) {
-		//updateGame(elapsedTime);
+		updateGame(elapsedTime);
 	}
 	drawGame();
 
-	if (curState === GAME_START_STATE || curState === GAME_OVER_STATE) {
+	if (curState !== GAME_PLAY_STATE ) {
 		background(51, 51, 51, 200);
 	}
 
@@ -137,6 +217,8 @@ function mouseClicked() {
 	toolManager.mouseClicked();
 	uiManager.mouseClicked();
 }
+
+function mouseMoved() {}
 
 function keyPressed() {
 	if (curState !== GAME_PLAY_STATE) {
@@ -157,5 +239,6 @@ function keyPressed() {
 		movePlayer('RIGHT');
 	} else if (keyCode === 32) {
 		// SPACE
+		fight();
 	}
 }
--- Hunt/world.js
+++ Hunt/world.js
@@ -1,12 +1,18 @@
 class World {
 	constructor(sizeX, sizeY) {
         this.size = { X: sizeX, Y: sizeY };
-        this.holes = [];
+		this.holes = [];
+		this.player = null;
+		this.animals = [];
+		this.selectedTile = null;
+		this.food = 0;
+		this.level = 1;
+
+		this.nextTileLevel = {X: 4, Y: 0};
 	}
 
 	draw() {
-		stroke(0);
-		fill(20, 180, 80);
+		// tiles
 		for (let i = 0; i < this.size.X; i++) {
 			for (let j = 0; j < this.size.Y; j++) {
 				const idx = this.getIndex(i,j);
@@ -17,13 +23,40 @@ class World {
 				}
 			}
 		}
+		// exit
+		const x = this.nextTileLevel.X * tileSize;
+		const y = this.nextTileLevel.Y * tileSize;
+		stroke(0);
+		fill(20, 180, 80);
+		rect(x+2,y+2,tileSize-4, tileSize-4);
+		// entities
+		this.animals.forEach(animal=>animal.draw());
+		this.player.draw();
 	}
 
-    update() {}
-    
+	update(elapsedTime) {
+		this.player.update(elapsedTime);
+		this.animals.forEach(animal=>animal.update(elapsedTime));
+	}
+
+	mouseMoved(mx,my) {
+		this.selectedTile = this.getTile(mx,my);
+	}
+
     getIndex(tileX, tileY) {
         return tileX + tileY * this.size.X;
-    }
+	}
+	getTile(x,y) {
+		const X = Math.floor(x/tileSize);
+		const Y = Math.floor(y/tileSize);
+		if( X < 0 || X >= this.size.X) {
+			return null;
+		}
+		if( Y < 0 || Y >= this.size.Y) {
+			return null;
+		}
+		return {X ,Y };
+	}
 
 	isFree(tileX, tileY) {
 		if (tileX < 0 || tileY < 0 || tileX >= this.size.X || tileY >= this.size.Y) {
@@ -31,9 +64,29 @@ class World {
 		}
 		return !this.holes.includes(this.getIndex(tileX, tileY));
 	}
+
+	move() {
+		// move all animals
+		this.animals.forEach(animal=>animal.move());
+	}
+
+	hurt(tileX, tileY, damage) {
+		this.animals.forEach(animal=> {
+			if( animal.tilePosition.X === tileX && animal.tilePosition.Y === tileY ) {
+				animal.hurt(damage);
+			}
+		});
+	}
+
+	nextLevel() {
+		this.level++;
+		this.animals = [];
+		this.player.setTileX(playerStart.X);
+		this.player.setTileY(playerStart.Y);
+	}
 }
 
-class Player extends Sprite {
+class Entity extends Sprite {
 	constructor(tileX, tileY) {
 		super(tileX * tileSize, tileY * tileSize);
 		this.tilePosition = { X: tileX, Y: tileY };
@@ -46,5 +99,57 @@ class Player extends Sprite {
 	setTileY(tileY) {
 		this.tilePosition.Y = tileY;
 		this.position.y = tileY * tileSize;
+	}
+}
+
+class Player extends Entity {
+	constructor(tileX, tileY) {
+		super(tileX, tileY);
+		this.addAnimation('idle', 'player01', [ 0, 1, 2, 3 ], FPS, true);
+		this.addAnimation('up', 'player01', [ 4, 5, 6, 7 ], FPS, true);
+	}
+
+	getDamage() {
+		return 5;
+	}
+}
+
+class Animal extends Entity {
+	constructor(name, tileX, tileY) {
+		super(tileX, tileY);
+		this.addAnimation('idle', name, [ 0, 1, 2, 3 ], FPS, true);
+		this.addAnimation('dead', name, [ 4 ], FPS, true);
+		this.life = 5;
+	}
+
+	move() {
+		if( this.life === 0 ) {
+			// animal dead: cannot move
+			return;
+		}
+		// randomly move this animal
+		const dx = getRandomIntInclusive(-1,1);
+		const dy = dx === 0 ? getRandomIntInclusive(-1,1) : 0;
+		if( world.isFree(this.tilePosition.X+dx, this.tilePosition.Y+dy) ) {
+			this.setTileX(this.tilePosition.X+dx);
+			this.setTileY(this.tilePosition.Y+dy);
+		}
+	}
+
+	hurt(damage) {
+		if( this.life === 0 ) {
+			return;
+		}
+		this.life = Math.max(0, this.life-damage);
+		if( this.life === 0 ) {
+			// animal is dead, add food to player
+			world.food += 50;
+			this.playAnimation("dead");
+			playSound(animalDeathSound);
+			if( !firstBlood ) {
+				firstBlood = true;
+				uiManager.addLogger('Each kill gains food');
+			}
+		}
 	}
 }
