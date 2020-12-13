e3d45c11227f95963bf71c41eff45f50f3079930
Stephane <stephane.conreaux@yahoo.fr>
2020-12-13 11:28:04 +0100

    mute all

--- GameEngine/sound.js
+++ GameEngine/sound.js
@@ -1,22 +1,31 @@
 class sndMgr {
 	constructor() {
 		this.sounds = {};
+		this.masterVolume = 1;
 	}
 
-	addSound(name, sound, volume=1) {
+	mute(value) {
+		if (value) {
+			masterVolume(0);
+		} else {
+			masterVolume(this.masterVolume);
+		}
+	}
+
+	addSound(name, sound, volume = 1) {
 		sound.setVolume(volume);
 		this.sounds[name] = sound;
 	}
 
-	playSound(name, rate=1) {
+	playSound(name, rate = 1) {
 		const sound = this.sounds[name];
 		if (sound) {
-            sound.rate(rate);
-            sound.play();
-        }
-    }
-    
-    stopSound(name) {
+			sound.rate(rate);
+			sound.play();
+		}
+	}
+
+	stopSound(name) {
 		const sound = this.sounds[name];
 		if (sound) sound.stop();
 	}
