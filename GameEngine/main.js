14ea1cf8ad3a7d59d02cc64b432d12e2364eb7ac
Stephane <stephane.conreaux@yahoo.fr>
2020-12-20 09:45:22 +0100

    improve game engine startup

--- GameEngine/main.js
+++ GameEngine/main.js
@@ -1,21 +1,44 @@
 const uiManager = new UIManager();
-uiManager.loggerContainer = new LoggerContainer(200, 200, 240, 100);
+const windowWidth = 800;
+const windowHeight = 600;
+uiManager.loggerContainer = new LoggerContainer(windowWidth-300, windowHeight-100, 240, 100);
 uiManager.loggerContainer.visible = true;
 
 const toolManager = new ToolManager();
 const jobManager = new JobManager();
+const soundManager = new SoundMgr();
+const spritesheet = new SpriteSheet();
 
+const GAME_LOADING_STATE = 0;
 const GAME_START_STATE = 1;
 const GAME_PLAY_STATE = 2;
 const GAME_OVER_STATE = 3;
-let curState = GAME_START_STATE;
+let curState = GAME_LOADING_STATE;
 
 let lastTime = 0;
 
 function preload() {
 }
 
+function musicClicked() {
+	// TODO
+}
+
+function speakerClicked() {
+	speakerButton.checked = !speakerButton.checked;
+	soundManager.mute(!speakerButton.checked);
+}
+
+const speakerButton = new BFloatingButton(windowWidth - 70 - 10 - 70, 70, '\uD83D\uDD0A', speakerClicked);
+const musicButton = new BFloatingButton(windowWidth - 70, 70, '\uD83C\uDFB6', musicClicked);
+
 function initUI() {
+    speakerButton.setTextSize(50);
+	musicButton.setTextSize(50);
+	musicButton.enabled = false;
+	musicButton.checked = false;
+	const menu = [ speakerButton, musicButton ];
+	uiManager.setUI(menu);
 }
 
 function setup() {
@@ -25,7 +48,6 @@ function setup() {
 
     frameRate(60);
 
-    uiManager.addLogger('This is a logger');
     lastTime = Date.now();
 }
 
@@ -37,10 +59,37 @@ function drawGame() {
 
 }
 
+function initGame() {
+
+}
+
+function drawLoading() {
+	fill(0);
+	noStroke();
+	textSize(50);
+	textAlign(CENTER, CENTER);
+	text('Loading...', width / 2, height / 2);
+	if (
+		soundManager.maxLoadedSounds === soundManager.maxLoadingSounds &&
+		spritesheet.maxLoadedImages === spritesheet.maxLoadingImages
+	) {
+		curState = GAME_START_STATE;
+
+        // init game
+        initGame();
+		textAlign(LEFT, BASELINE);
+		uiManager.addLogger('Game loaded');
+	}
+}
+
 function draw() {
     const currentTime = Date.now();
 	const elapsedTime = currentTime - lastTime;
     background(51);
+    if (curState === GAME_LOADING_STATE) {
+		drawLoading();
+		return;
+	}
 
     uiManager.processInput();
 
