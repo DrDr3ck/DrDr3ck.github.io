b2b4ed10c5ba457661838a1a6ed3d2fe0ce0fddf
Stephane <stephane.conreaux@yahoo.fr>
2022-05-12 18:47:57 +0200

    more names

--- Cartographer/city-name.js
+++ Cartographer/city-name.js
@@ -1,195 +1,10 @@
 // https://www.fantasynamegenerators.com/yourArt.php
 
-/*
-Poignan
-Auberluire
-Haguelet
-Maugnan
-Antomomble
-Bormart
-Péritou
-Martirgues
-Belrac
-Aurilimar
-Bourlimar
-Carcalliers
-Soicourt
-Aurirac
-Coloville
-Aubermur
-Chollon
-Martimasse
-Montnesse
-Bourmomble
-Argenvin
-Vagnan
-Villeurtou
-Soission
-Lippe
-Nanlun
-Fréppe
-Beauluçon
-Argenseau
-Chanoît
-Puzon
-Vierteaux
-Martilon
-Bourbeuge
-Plaiteaux
-Mariville
-Besanbagne
-Béssion
-Valès
-Chally
-Drathune
-Roallac
-Borbagne
-Bornin
-Maully
-Plaissy
-Angoumasse
-Perpillac
-Borlet
-Pasier
-Montris
-Argenluire
-Levasir
-Nanmart
-Martibagne
-Roaville
-Levaves
-Rounoît
-Montyonne
-Carcassion
-bellvault
-castlehand
-limehaven
-maplehorn
-timberbreak
-honeywell
-wildebreak
-sandfair
-summermond
-lostbreak
-grimewatch
-falseford
-silverreach
-houndmount
-hollowfort
-cursecairn
-baybreach
-angelvalley
-bouldermere
-sandpass
-blindshade
-lagoonward
-wildward
-hazelbay
-bellshire
-gloomgate
-frostrun
-embermaw
-ebonscar
-baygrove
-dogyard
-castlebourne
-timberbreak
-starfell
-ragesummit
-falsehand
-knighttown
-autumnfort
-thornwick
-swamphaven
-
-
-The Clay Cliffs
-The Pinetree Ravine
-The Distant Cliff
-The Bellowing Fjords
-The Bellowing Fjords
-The Faraway Wall
-Rothely Cliffs
-Bordtois Cliff
-Eatogan Crag
-Westrath Cliffs
-
- mom Hill
-grurk Hill
-rowlob Square
-limot District
-splotteos Vale
-South hafisp
-kiafop Yard
-Lower South yicaint
-jedutmus Side
-East bruwonwic Road
-
-Vault of Oblivion
-Borough of Desertion
-Cove of Shadows
-Town of the Damned
-Borough of Water
-The Cracking Village
-The Obsidian River
-The Cracking Temple
-The Remnant Town
-The Sleeping Reef
-
-le Havre Neigeux
-l'Archipel Brillant
-la Péninsule Ancienne
-l'Île Éclairée
-la Péninsule Invisible
-l'Île du Roatou
-l'Île du Talet
-le Récif du Caseau
-la Chaîne de la Pagny
-l'Atoll de la Plainoît
-
-le Mur du Navire Coulé
-les Canyons Diaboliques
-le Ravin Agité
-les Abîmes de Granit
-les Promontoires de Vulpin
-les Murs du Navire Brisé
-l'Abysse de Maitou
-le Promontoire de la Bounau
-le Gouffre d'Argenvers
-les Murs du Tally
-
-la Péninsule d'Oasis
-l'Île de Méduse
-l'Île Pourrie
-l'Île de Rugissement
-l'Enclave Perpétuelle
-le Havre de Vierrault
-l'Île des Clalliers
-l'Île de la Dives
-l'Enclave des Courmans
-la Péninsule de la Perpibeuge
-
-le Gouffre de Pêcheurs
-le Rocher Dentelé
-l'Abysse Arctique
-le Rocher Isolé
-le Rocher Mélancolique
-le Promontoire de Granit
-les Abîmes de la Martissonne
-l'Abysse du Maugnane
-l'Abysse de Vinrac
-l'Abîme de la Vitrovin
-*/
-
 const MASCULIN = "masculin";
 const FEMININ = "feminin";
 
 const SINGULIER = "1";
 const PLURIEL = "100";
-
-// <a href="mailto:nom@adresse?subject=Titre&body=copie ton image ici en faisant CTRL+V">ici</a>
-
-// Ecartee Precaire Malveillante Etroit Troublante Nue Impossible Monotone Corrompue Effrayante Ardents Obscure Sauvage
 
 var suffixes = [
 	{genre: MASCULIN, nombre: SINGULIER, name: "Neigeux"},
@@ -242,7 +57,14 @@ var suffixes = [
 	{genre: FEMININ, nombre: SINGULIER, name: "Noire"},
 	{genre: MASCULIN, nombre: PLURIEL, name: "Noirs"},
 	{genre: FEMININ, nombre: PLURIEL, name: "Noires"},
-	{name: "Mélancolique"},
+	{genre: MASCULIN, nombre: SINGULIER, name: "Perpétuel"},
+	{genre: FEMININ, nombre: SINGULIER, name: "Perpétuelle"},
+	{genre: MASCULIN, nombre: PLURIEL, name: "Perpétuels"},
+	{genre: FEMININ, nombre: PLURIEL, name: "Perpétuelles"},
+	{genre: MASCULIN, nombre: SINGULIER, name: "Mélancolique"},
+	{genre: FEMININ, nombre: SINGULIER, name: "Mélancolique"},
+	{genre: MASCULIN, nombre: PLURIEL, name: "Mélancoliques"},
+	{genre: FEMININ, nombre: PLURIEL, name: "Mélancoliques"},
 	{name: "de Cristal"},
 	{name: "du Roatou"},
 	{name: "du Talet"},
@@ -261,7 +83,6 @@ var suffixes = [
 	{name: "d'Oasis"},
 	{name: "de Méduse"},
 	{name: "de Rugissement"},
-	{name: "Perpétuelle"},
 	{name: "de Vierrault"},
 	{name: "des Clalliers"},
 	{name: "de la Dives"},
@@ -275,6 +96,38 @@ var suffixes = [
 	{name: "de Vinrac"},
 	{name: "de la Vitrovin"}
 ];
+
+function addAdjectif(adjectif) {
+	return [
+		{genre: MASCULIN, nombre: SINGULIER, name: adjectif},
+		{genre: FEMININ, nombre: SINGULIER, name: `${adjectif}e`},
+		{genre: MASCULIN, nombre: PLURIEL, name: `${adjectif}s`},
+		{genre: FEMININ, nombre: PLURIEL, name: `${adjectif}es`}
+	];
+}
+
+function addAdjectifNeutre(adjectif) {
+	return [
+		{genre: MASCULIN, nombre: SINGULIER, name: adjectif},
+		{genre: FEMININ, nombre: SINGULIER, name: `${adjectif}`},
+		{genre: MASCULIN, nombre: PLURIEL, name: `${adjectif}s`},
+		{genre: FEMININ, nombre: PLURIEL, name: `${adjectif}s`}
+	];
+}
+
+suffixes.push(...addAdjectif("Ecarté"));
+suffixes.push(...addAdjectif("Malveillant"));
+suffixes.push(...addAdjectif("Etroit"));
+suffixes.push(...addAdjectif("Troublant"));
+suffixes.push(...addAdjectif("Nu"));
+suffixes.push(...addAdjectif("Corrompu"));
+suffixes.push(...addAdjectif("Effrayant"));
+suffixes.push(...addAdjectif("Ardent"));
+suffixes.push(...addAdjectif("Obscur"));
+suffixes.push(...addAdjectifNeutre("Sauvage"));
+suffixes.push(...addAdjectifNeutre("Monotone"));
+suffixes.push(...addAdjectifNeutre("Impossible"));
+suffixes.push(...addAdjectifNeutre("Précaire"));
 
 var prefixes = [
 	{genre: MASCULIN, nombre: SINGULIER, name:"l'Archipel"},
@@ -327,7 +180,7 @@ function getRandomName() {
 	var pre = prefixes[Math.floor(Math.random() * prefixes.length)];
 	var filteredSuffix = suffixes.filter(s=>(s.genre === pre.genre && s.nombre === pre.nombre) || !s.genre);
 	var prefix = pre.name;
-	if( pre.nombre > 1 && Math.random() > 0.3 ) {
+	if( pre.nombre == PLURIEL && Math.random() > 0.3 ) {
 		// add a number between 'les' and the word
 		const size = [
 			"Deux", "Trois", "Quatre", "Cinq", "Six", "Sept", "Huit", "Neuf",
@@ -335,13 +188,28 @@ function getRandomName() {
 			"Cents", "Six Cent Soixante Six", "Mille", "Six Milles"
 		];
 		const s = size[Math.floor(Math.random() * size.length)];
-		const words = pre.name.split(" ");
+		const words = pre.name.split(" "); 
 		words.splice(1,0,s);
 		prefix = words.join(" ");
+	}
+	if( pre.nombre === SINGULIER && Math.random() > 0.3 ) {
+		const words = pre.name.split(" ");
+		if( words[0] === "le" ) {
+			const adjs = ["Grand", "Magnifique", "Dernier", "Vieux", "Premier", "Second", "Petit", "Minuscule"];
+			const adj = adjs[Math.floor(Math.random() * adjs.length)];
+			words.splice(1,0,adj);
+			prefix = words.join(" ");
+		} else if( words[0] === "la" ) {
+			const adjs = ["Grande", "Belle", "Dernière", "Vieille", "Première", "Seconde", "Petite", "Minuscule"];
+			const adj = adjs[Math.floor(Math.random() * adjs.length)];
+			words.splice(1,0,adj);
+			prefix = words.join(" ");
+		}
 	}
 	var suf = filteredSuffix[Math.floor(Math.random() * filteredSuffix.length)];
 	return `${prefix} ${suf.name}`;
 }
+
 
 const names= [];
 for(let i=0; i < 100; i++) {
@@ -354,3 +222,4 @@ for(let i=0; i < 100; i++) {
 }
 
 console.log(names);
+
