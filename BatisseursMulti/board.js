bde6b8a62095262de3182956594f539f4937b28a
Stephane <stephane.conreaux@yahoo.fr>
2022-10-23 15:24:14 +0200

    Batisseur: images and initialization

---
+++ BatisseursMulti/board.js
@@ -0,0 +1,199 @@
+class Board {
+    constructor() {
+        this.chantiers = [];
+        this.team = [allOuvriers.pop()];
+        this.points = 0;
+        this.ecus = 10;
+
+        this.actions = [];
+    }
+
+    addOuvrier(ouvrier) {
+        this.team.push(ouvrier);
+    }
+
+    addChantier(chantier) {
+        this.chantiers.push(chantier);
+    }
+
+    workOuvrier(ouvrier, chantierName) {
+        const chantier = findChantier(chantierName);
+        chantier.addOuvrier(ouvrier);
+    }
+
+    takeEcus() {
+        if( this.actions.length === 1 ) {
+            this.ecus += 1;
+        }
+        if( this.actions.length === 2 ) {
+            this.ecus += 3;
+        }
+        if( this.actions.length === 3 ) {
+            this.ecus += 6;
+        }
+    }
+}
+
+class Chantier {
+    constructor(name, index, materiaux, ecus, points) {
+        this.name = name;
+        this.index = index;
+        this.materiaux = materiaux;
+        this.ecus = ecus;
+        this.points = points;
+        this.ouvriers = [];
+    }
+
+    addOuvrier(ouvrier) {
+        this.ouvriers.push(ouvrier);
+        // check if Chantier is 'terminated'
+    }
+}
+
+class Ouvrier {
+    constructor(name, index, materiaux, ecus) {
+        this.name = name;
+        this.index = index;
+        this.materiaux = materiaux;
+        this.ecus = ecus;
+    }
+}
+
+const allOuvriers = [];
+const allChantiers = [];
+const allMachines = [];
+
+function addOuvrier(name, materiaux, ecus) {
+    allOuvriers.push(new Ouvrier(name, allOuvriers.length, materiaux, ecus));
+}
+
+function addChantier(name, materiaux, ecus, points) {
+    allChantiers.push(new Chantier(name, allChantiers.length, materiaux, ecus, points));
+}
+
+function addMachine(name, materiauxChantier, points, materiauxOuvrier) {
+    allMachines.push(
+        {
+            name,
+            materiauxChantier,
+            points,
+            materiauxOuvrier
+        }
+    )
+}
+
+addMachine("Un four à tuiles", {pierre: 2, bois: 1}, 2, {tuile: 3});
+addMachine("Un four à tuiles", {bois: 1, savoir: 1}, 1, {tuile: 2});
+addMachine("Une scie circulaire", {pierre: 1, savoir: 1}, 1, {bois: 2});
+addMachine("Une scie circulaire", {savoir: 2, tuile: 1}, 2, {bois: 3});
+addMachine("Un instrument de mesure", {pierre: 1, bois: 1}, 1, {savoir: 2});
+addMachine("Un instrument de mesure", {pierre: 1, tuile: 2}, 2, {savoir: 3});
+addMachine("Une grue", {bois: 2, savoir: 1}, 2, {pierre: 3});
+addMachine("Une grue", {bois: 1, tuile: 1}, 1, {pierre: 2});
+
+// 6
+addOuvrier("APPRENTI", {pierre: 1, bois: 1, savoir: 0, tuile: 0}, 2);
+addOuvrier("APPRENTI", {pierre: 1, bois: 0, savoir: 1, tuile: 0}, 2);
+addOuvrier("APPRENTI", {pierre: 1, bois: 0, savoir: 0, tuile: 1}, 2);
+addOuvrier("APPRENTI", {pierre: 0, bois: 1, savoir: 1, tuile: 0}, 2);
+addOuvrier("APPRENTI", {pierre: 0, bois: 1, savoir: 0, tuile: 1}, 2);
+addOuvrier("APPRENTI", {pierre: 0, bois: 0, savoir: 1, tuile: 1}, 2);
+
+// 12
+addOuvrier("MANOEUVRE", {pierre: 2, bois: 1, savoir: 0, tuile: 0}, 3);
+addOuvrier("MANOEUVRE", {pierre: 2, bois: 0, savoir: 1, tuile: 0}, 3);
+addOuvrier("MANOEUVRE", {pierre: 2, bois: 0, savoir: 0, tuile: 1}, 3);
+addOuvrier("MANOEUVRE", {pierre: 1, bois: 2, savoir: 0, tuile: 0}, 3);
+addOuvrier("MANOEUVRE", {pierre: 1, bois: 0, savoir: 2, tuile: 0}, 3);
+addOuvrier("MANOEUVRE", {pierre: 1, bois: 0, savoir: 0, tuile: 2}, 3);
+addOuvrier("MANOEUVRE", {pierre: 0, bois: 2, savoir: 1, tuile: 0}, 3);
+addOuvrier("MANOEUVRE", {pierre: 0, bois: 2, savoir: 0, tuile: 1}, 3);
+addOuvrier("MANOEUVRE", {pierre: 0, bois: 1, savoir: 2, tuile: 0}, 3);
+addOuvrier("MANOEUVRE", {pierre: 0, bois: 1, savoir: 0, tuile: 2}, 3);
+addOuvrier("MANOEUVRE", {pierre: 0, bois: 0, savoir: 2, tuile: 1}, 3);
+addOuvrier("MANOEUVRE", {pierre: 0, bois: 0, savoir: 1, tuile: 2}, 3);
+
+// 16
+addOuvrier("COMPAGNON", {pierre: 3, bois: 1, savoir: 0, tuile: 0}, 4);
+addOuvrier("COMPAGNON", {pierre: 2, bois: 2, savoir: 0, tuile: 0}, 4);
+addOuvrier("COMPAGNON", {pierre: 2, bois: 1, savoir: 1, tuile: 0}, 4);
+addOuvrier("COMPAGNON", {pierre: 2, bois: 0, savoir: 2, tuile: 0}, 4);
+addOuvrier("COMPAGNON", {pierre: 2, bois: 0, savoir: 0, tuile: 2}, 4);
+addOuvrier("COMPAGNON", {pierre: 1, bois: 2, savoir: 0, tuile: 1}, 4);
+addOuvrier("COMPAGNON", {pierre: 1, bois: 1, savoir: 1, tuile: 1}, 4);
+addOuvrier("COMPAGNON", {pierre: 1, bois: 1, savoir: 1, tuile: 1}, 4);
+addOuvrier("COMPAGNON", {pierre: 1, bois: 0, savoir: 3, tuile: 0}, 4);
+addOuvrier("COMPAGNON", {pierre: 1, bois: 0, savoir: 2, tuile: 1}, 4);
+
+addOuvrier("COMPAGNON", {pierre: 0, bois: 3, savoir: 0, tuile: 1}, 4);
+addOuvrier("COMPAGNON", {pierre: 0, bois: 2, savoir: 2, tuile: 0}, 4);
+addOuvrier("COMPAGNON", {pierre: 0, bois: 2, savoir: 0, tuile: 2}, 4);
+addOuvrier("COMPAGNON", {pierre: 0, bois: 1, savoir: 1, tuile: 2}, 4);
+addOuvrier("COMPAGNON", {pierre: 0, bois: 0, savoir: 2, tuile: 2}, 4);
+addOuvrier("COMPAGNON", {pierre: 0, bois: 0, savoir: 1, tuile: 3}, 4);
+
+// 8
+addOuvrier("MAITRE", {pierre: 3, bois: 2, savoir: 0, tuile: 0}, 5);
+addOuvrier("MAITRE", {pierre: 3, bois: 0, savoir: 0, tuile: 2}, 5);
+addOuvrier("MAITRE", {pierre: 2, bois: 3, savoir: 0, tuile: 0}, 5);
+addOuvrier("MAITRE", {pierre: 2, bois: 0, savoir: 0, tuile: 3}, 5);
+addOuvrier("MAITRE", {pierre: 0, bois: 3, savoir: 2, tuile: 0}, 5);
+addOuvrier("MAITRE", {pierre: 0, bois: 2, savoir: 3, tuile: 0}, 5);
+addOuvrier("MAITRE", {pierre: 0, bois: 0, savoir: 3, tuile: 2}, 5);
+addOuvrier("MAITRE", {pierre: 0, bois: 0, savoir: 2, tuile: 3}, 5);
+
+// 6 ecus
+addChantier("La hutte de paille", {pierre: 1, tuile: 1}, 6, 1);
+addChantier("Le lavoir", {bois: 1, tuile: 2}, 6, 1);
+addChantier("La cabane perchée", {bois: 2, savoir: 1}, 6, 1);
+addChantier("Le pont couvert", {bois: 1, savoir: 2}, 6, 1);
+addChantier("Le pont en pierre", {pierre: 2, savoir: 1}, 6, 1);
+// 8 ecus
+addChantier("La tonnelle", {pierre: 1, tuile: 1}, 8, 0);
+addChantier("La cabane", {bois: 1, savoir: 1}, 8, 0);
+// 10 ecus
+addChantier("La maison urbaine", {pierre: 2, savoir: 2, tuile: 1}, 10, 2);
+addChantier("La chaumière", {bois: 2, savoir: 1, tuile: 2}, 10, 2);
+addChantier("La maison rurale", {pierre: 1, bois: 2, savoir: 1, tuile: 1}, 10, 2);
+addChantier("La maisonnette", {pierre: 2, bois: 1, tuile: 2}, 10, 2);
+// 12 ecus
+addChantier("Le silo à grains", {bois: 2, savoir: 3, tuile: 1}, 12, 3);
+addChantier("L'auberge'", {pierre: 2, bois: 1, savoir: 1, tuile: 2}, 12, 3);
+addChantier("Le relais rural", {bois: 3, savoir: 1, tuile: 2}, 12, 3);
+addChantier("L'étable", {bois: 1, savoir: 2, tuile: 3}, 12, 3);
+addChantier("La porcherie", {bois: 2, savoir: 2, tuile: 2}, 12, 3);
+// 14 ecus
+addChantier("La forge", {pierre: 2, bois: 2, tuile: 3}, 14, 3);
+addChantier("Le mouline à vent", {pierre: 3, savoir: 3, tuile: 1}, 14, 3);
+addChantier("L'écurie'", {bois: 3, savoir: 1, tuile: 3}, 14, 3);
+addChantier("Le mouline à eau", {bois: 2, savoir: 3, tuile: 2}, 14, 3);
+// 16 ecus
+addChantier("L'hotel)", {pierre: 3, savoir: 3, tuile: 1}, 16, 4);
+addChantier("La ferme)", {pierre: 4, bois: 2, tuile: 2}, 16, 4);
+addChantier("La taverne)", {pierre: 1, bois: 3, savoir: 1, tuile: 3}, 16, 4);
+addChantier("Le relais postal)", {pierre: 3, bois: 1, savoir: 2, tuile: 2}, 16, 4);
+addChantier("Les halles)", {bois: 3, savoir: 2, tuile: 3}, 16, 4);
+addChantier("La maison bourgeoise)", {pierre: 2, bois: 2, savoir: 2, tuile: 2}, 16, 4);
+// 18 ecus
+addChantier("Le cloitre)", {pierre: 4, bois: 2, savoir: 4}, 18, 5);
+addChantier("L'église)", {pierre: 4, savoir: 2, tuile: 4}, 18, 5);
+addChantier("La tour de guet)", {pierre: 3, bois: 3, savoir: 2, tuile: 2}, 18, 5);
+addChantier("L'abbaye')", {pierre: 4, bois: 3, savoir: 2, tuile: 1}, 18, 5);
+addChantier("La chapelle)", {pierre: 3, bois: 2, savoir: 2, tuile: 3}, 18, 5);
+// 20 ecus
+addChantier("La cathédrale)", {pierre: 5, bois: 4, savoir: 4, tuile: 4}, 20, 8);
+addChantier("Le chateau fort)", {pierre: 5, bois: 3, savoir: 5, tuile: 3}, 20, 7);
+addChantier("L'aqueduc')", {pierre: 5, bois: 2, savoir: 5}, 20, 6);
+
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+const shuffleArray = (array) => {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i+1))
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+shuffleArray(allOuvriers);
+shuffleArray(allChantiers);
--- BatisseursMulti/main.js
+++ BatisseursMulti/main.js
@@ -1,6 +1,6 @@
 const uiManager = new UIManager();
-const windowWidth = 1400;
-const windowHeight = 800;
+const windowWidth = 1800;
+const windowHeight = 900;
 uiManager.loggerContainer = new LoggerContainer(windowWidth-300, windowHeight-100, 240, 100);
 uiManager.loggerContainer.visible = true;
 
@@ -17,8 +17,12 @@ let curState = GAME_LOADING_STATE;
 let toggleDebug = false;
 let lastTime = 0;
 
+const boards = [];
+
+const meepleSize = 150;
+
 function preload() {
-	spritesheet.addSpriteSheet('meeple', './meeple.png', tileSize, tileSize);
+	spritesheet.addSpriteSheet('meeple', './meeple.png', meepleSize, meepleSize);
 }
 
 function musicClicked() {
@@ -26,6 +30,7 @@ function musicClicked() {
 }
 
 let socket;
+let playerIndex = 0;
 
 const speakerStorageKey = 'DrDr3ck/GameEngine/Speaker';
 function speakerClicked() {
@@ -38,6 +43,8 @@ function startClicked() {
 	curState = GAME_PLAY_STATE;
 	uiManager.setUI([ speakerButton, musicButton ]);
 	uiManager.addLogger("Start game");
+
+	boards.push( new Board());
 }
 
 const speakerButton = new BFloatingSwitchButton(windowWidth - 70 - 10 - 70, 70, '\uD83D\uDD0A', speakerClicked);
@@ -64,6 +71,12 @@ function setup() {
     canvas.parent('canvas');
 
 	spritesheet.addSpriteSheet('ouvriers', './ouvriers.png', 230, 300);
+	spritesheet.addSpriteSheet('batiments', './batiments.png', 325, 300);
+
+	spritesheet.addSpriteSheet('ecu', './ecu.png', 82, 82);
+
+	spritesheet.addSpriteSheet('next_turn', './next_turn.png', 140, 140);
+	spritesheet.addSpriteSheet('couronne', './couronne.png', 80, 50);
 
 	socket = io.connect('http://localhost:3000');
 
@@ -76,8 +89,126 @@ function updateGame(elapsedTime) {
 
 }
 
-function drawGame() {
+const scale = 0.55;
+let points = 0;
+let ecus = 10;
 
+function emit(type, data) {
+	const board = boards[playerIndex];
+	// socket.emit(type, data);
+	if( type === "OuvrirChantier" ) {
+		
+	}
+	if( type === "RecruterOuvrier" ) {
+
+	}
+	if( type === "TravaillerOuvrier" ) {
+
+	}
+	if( type === "PrendreEcus" ) {
+
+	}
+}
+
+function drawActionCounter() { 
+	push();
+	stroke(0);
+	strokeWeight(3);
+	fill(255,242,0); // Jaune
+	fill(34,177,76); // Vert
+	const board = boards[playerIndex];
+	if( board.actions.length === 3 ) {
+		fill(237,28,36); // Rouge
+	}
+	ellipse(1620, 200, 40);
+	if( board.actions.length === 2 ) {
+		fill(237,28,36);
+	}
+	ellipse(1520, 200, 40);
+	if( board.actions.length === 1 ) {
+		fill(237,28,36);
+	}
+	ellipse(1420, 200, 40);
+	pop();
+}
+
+function drawTeam(team) {
+	team.forEach((ouvrier, i)=>
+		spritesheet.drawScaledSprite("ouvriers", ouvrier.index, 10+(230*scale+10)*i, windowHeight-10-scale*300, scale)
+	);
+}
+
+function drawChantier() {
+	push();
+	stroke(0);
+	strokeWeight(2);
+	fill(232,212,183);
+	rect(5,360-5,680,300*scale+10,5);
+	rect(695,360-5,680,300*scale+10,5);
+	rect(5,665-65-60,680,300*scale+10,5);
+	rect(695,665-65-60,680,300*scale+10,5);
+
+	const board = boards[playerIndex];
+	if( board.chantiers[0] ) {
+		const chantier = board.chantiers[0];
+		spritesheet.drawScaledSprite("batiments", chantier.index, 10, 360, scale);
+		/*
+		spritesheet.drawScaledSprite("ouvriers", 0, 10+(325*scale+10), 360, scale);
+		spritesheet.drawScaledSprite("ouvriers", 1, 10+(325*scale+10)+40, 360, scale);
+		spritesheet.drawScaledSprite("ouvriers", 2, 10+(325*scale+10)+80, 360, scale);
+		*/
+	}
+	if( board.chantiers[1] ) {
+		const chantier = board.chantiers[1];
+		spritesheet.drawScaledSprite("batiments", chantier.index, 700, 360, scale);
+	}
+
+	if( board.chantiers[2] ) {
+		const chantier = board.chantiers[2];
+		spritesheet.drawScaledSprite("batiments", chantier.index, 10, 670-65-60, scale);
+	}
+
+	if( board.chantiers[3] ) {
+		const chantier = board.chantiers[3];
+		spritesheet.drawScaledSprite("batiments", chantier.index, 700, 670-65-60, scale);
+	}
+
+	pop();
+}
+
+function drawMeeples() {
+	push();
+	stroke(0);
+	noFill();
+	for( let i=0; i < 4; i++ ) {
+		spritesheet.drawScaledSprite("meeple", i, 1600, 400+i*meepleSize*0.8,0.8);
+	}
+	rect(1600, 400, meepleSize*0.8, meepleSize*0.8, 5);
+	pop();
+}
+
+function drawGame() {
+	for( let i=0; i < 6; i++ ) {
+		spritesheet.drawScaledSprite("batiments", allChantiers[i].index, 10+(325*scale+10)*i, 10, scale);
+		spritesheet.drawScaledSprite("ouvriers", allOuvriers[i].index, 10+(325*scale+10)*i, 10+315*scale, scale);
+	}
+
+	drawActionCounter();
+
+	spritesheet.drawSprite("next_turn", 0, 1450, 15);
+
+	textAlign(CENTER, CENTER);
+	spritesheet.drawSprite("ecu", 0, 1400, 250);
+	fill(180);
+	text(boards[playerIndex].ecus,1520,310);
+	spritesheet.drawScaledSprite("couronne", 0, 1600, 250, 2);
+	fill(20);
+	text(boards[playerIndex].points,1650,310);
+
+	drawTeam(boards[playerIndex].team);
+	drawChantier();
+
+	drawMeeples();
 }
 
 function initGame() {
@@ -137,6 +268,10 @@ function mouseClicked() {
 	if( toggleDebug ) {
 		uiManager.addLogger(`X=${mouseX}, Y=${mouseY}`);
 	}
+
+	// if over an ouvrier, take it
+	// if over a chantier, take it
+
 	toolManager.mouseClicked();
 	uiManager.mouseClicked();
 	return false;
@@ -145,5 +280,10 @@ function mouseClicked() {
 function keyPressed() {
 	if (key === "D") {
 		toggleDebug = !toggleDebug;
+	}
+
+	if( curState === GAME_START_STATE && key === 's' ) {
+		// Start
+		startClicked();
 	}
 }
