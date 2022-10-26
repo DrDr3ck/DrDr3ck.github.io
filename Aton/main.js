257bccc5b72083cdfa8e257d5fd9e982369f3cf6
Stephane <stephane.conreaux@yahoo.fr>
2022-10-26 18:49:59 +0200

    Aton: tiles and cards position

--- Aton/main.js
+++ Aton/main.js
@@ -1,6 +1,6 @@
 const uiManager = new UIManager();
 const windowWidth = 1400;
-const windowHeight = 800;
+const windowHeight = 900;
 uiManager.loggerContainer = new LoggerContainer(1170, windowHeight-100, windowWidth-1170, 100);
 uiManager.loggerContainer.visible = true;
 
@@ -14,6 +14,14 @@ const PLACECARD = "Place the card";
 const GAMEOVER = "End of game";
 let gameState = CHOOSECARD;
 
+const BLUE = 0;
+const RED = 1;
+
+const colors = [
+ {r:127,g:167,b:202},
+ {r:236,g:155,b:95}
+];
+
 const GAME_LOADING_STATE = 0;
 const GAME_START_STATE = 1;
 const GAME_PLAY_STATE = 2;
@@ -25,10 +33,71 @@ let lastTime = 0;
 
 let displayRules = false;
 
+const tileWidth = 35;
+const tilesPosition = [
+	[
+		{X:483,Y:173}, {X:541,Y:173}, {X:597,Y:173}, {X:652,Y:173},
+		{X:483,Y:233}, {X:541,Y:233}, {X:597,Y:233}, {X:652,Y:233},
+		{X:483,Y:293}, {X:541,Y:293}, {X:597,Y:293}, {X:652,Y:293}
+	],
+	[
+		{X:750,Y:173}, {X:808,Y:173}, {X:865,Y:173}, {X:919,Y:173},
+		{X:750,Y:233}, {X:808,Y:233}, {X:865,Y:233}, {X:919,Y:233},
+		{X:750,Y:293}, {X:808,Y:293}, {X:865,Y:293}, {X:919,Y:293}
+	],
+	[
+		{X:483,Y:480}, {X:541,Y:480}, {X:597,Y:480}, {X:652,Y:480},
+		{X:483,Y:536}, {X:541,Y:536}, {X:597,Y:536}, {X:652,Y:536},
+		{X:483,Y:595}, {X:541,Y:595}, {X:597,Y:595}, {X:652,Y:595}
+	],
+	[
+		{X:750,Y:480}, {X:808,Y:480}, {X:865,Y:480}, {X:919,Y:480},
+		{X:750,Y:536}, {X:808,Y:536}, {X:865,Y:536}, {X:919,Y:536},
+		{X:750,Y:595}, {X:808,Y:595}, {X:865,Y:595}, {X:919,Y:595}
+	],
+	[   // Kingdom of Dead
+		{X:475,Y:742},{X:475+62,Y:742},{X:475+62+62,Y:742},{X:475+62+62+62,Y:742},
+		{X:729,Y:742},{X:729+62,Y:742},{X:729+62+62,Y:742},{X:729+62+62+62,Y:742}
+	],
+	[   // Points
+		{X: 348, Y: 777}, // 0
+		{X: 404, Y: 760}, {X: 404, Y: 711}, {X: 404, Y: 659}, {X: 404, Y: 607}, {X: 404, Y: 555}, {X: 404, Y: 503}, {X: 404, Y: 451}, 
+		{X: 404, Y: 399}, {X: 404, Y: 351}, {X: 404, Y: 300}, {X: 404, Y: 249}, {X: 404, Y: 199}, {X: 404, Y: 146}, {X: 404, Y: 92}, {X: 404, Y: 40},
+		{X: 454, Y: 36}, {X: 509, Y: 36}, {X: 566, Y: 36}, {X: 622, Y: 36}, {X: 675, Y: 36},
+		{X: 728, Y: 36}, {X: 783, Y: 36}, {X: 837, Y: 36}, {X: 889, Y: 36}, {X: 944, Y: 36},
+
+		{X: 993, Y: 40}, {X: 993, Y: 92}, {X: 993, Y: 146}, {X: 993, Y: 199}, {X: 993, Y: 249}, {X: 993, Y: 300}, {X: 993, Y: 351}, {X: 993, Y: 399},
+		{X: 993, Y: 451}, {X: 993, Y: 503}, {X: 993, Y: 555}, {X: 993, Y: 607}, {X: 993, Y: 659}, {X: 993, Y: 711},	{X: 993, Y: 760}
+	]
+];
+
+// TODO: server
+const cards = [1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4];
 let board = null;
+let playerIndex = 0; // BLUE or RED
+
+const cardsPosition = [
+	[
+		{X:10, Y:45, width: 260, height:165}, {X:10, Y:45+190, width: 260, height:165},
+		{X:10, Y:45+190+190, width: 260, height:165}, {X:10, Y:45+190+190+190, width: 260, height:165}
+	],
+	[
+		{X:1130, Y:45, width: 260, height:165}, {X:1130, Y:45+190, width: 260, height:165},
+		{X:1130, Y:45+190+190, width: 260, height:165}, {X:1130, Y:45+190+190+190, width: 260, height:165}
+	]
+]
+
+const curCards = [
+	[],[] // #0 for player BLUE and #1 for player RED
+];
+
+let overCardIdx = -1;
+let selectedCardIdx = -1;
+
+const points = [0,0];
 
 function preload() {
-	spritesheet.addSpriteSheet('cover', './cover.jpg', 349, 424);
+	spritesheet.addSpriteSheet('cover', './cover.jpg', 450, 635);
 }
 
 function musicClicked() {
@@ -53,7 +122,7 @@ function startClicked() {
 
 const speakerButton = new BFloatingSwitchButton(windowWidth - 70 - 10 - 70, 70, '\uD83D\uDD0A', speakerClicked);
 const musicButton = new BFloatingSwitchButton(windowWidth - 70, 70, '\uD83C\uDFB6', musicClicked);
-const startButton = new BButton(80, windowHeight - 50 - 200, "START", startClicked);
+const startButton = new BButton(680, windowHeight - 50 - 200, "START", startClicked);
 
 const rulesButton = new BFloatingButton(windowWidth - 70, 70, '?', ()=>{
 	// display/hide rules
@@ -84,8 +153,8 @@ function setup() {
 	canvas = createCanvas(windowWidth, windowHeight);
     canvas.parent('canvas');
 
-	spritesheet.addSpriteSheet('cards', './cards.jpg', 165, 260);
-	spritesheet.addSpriteSheet('verso', './verso.png', 165, 260);
+	spritesheet.addSpriteSheet('cards', './cards.png', 260, 165);
+	spritesheet.addSpriteSheet('verso', './verso.png', 260, 165);
 	spritesheet.addSpriteSheet('board', './board.jpg', 799, 796);
 
     frameRate(20);
@@ -97,36 +166,121 @@ function updateGame(elapsedTime) {
 
 }
 
+const cardIndex = (type, value) => {
+	if( type === BLUE ) {
+		switch(value) {
+			case 1:
+				return 0; 
+			case 2:
+				return 2;
+			case 3:
+				return 4;
+		}
+		return 6;
+	}
+	if( type === RED ) {
+		switch(value) {
+			case 1:
+				return 1; 
+			case 2:
+				return 3;
+			case 3:
+				return 5;
+		}
+		return 7;
+	}
+}
+
+function drawCard(color, index, position) {
+	const cardPosition = position ? position : cardsPosition[color][index];
+	if( curCards[color][index].visible ) {
+		if( (color === playerIndex && index !== selectedCardIdx) || position ) {
+			spritesheet.drawSprite("cards", cardIndex(color,curCards[color][index].value), cardPosition.X, cardPosition.Y);
+		}
+	} else {
+		spritesheet.drawSprite("verso", color, cardPosition.X, cardPosition.Y);
+	}
+	if( index === overCardIdx && color === playerIndex ) {
+		rect(cardPosition.X, cardPosition.Y, cardPosition.width, cardPosition.height, 20);
+	}
+}
+
+function drawPoints() {
+	push();
+	strokeWeight(1);
+	stroke(0);
+	fill(colors[BLUE].r, colors[BLUE].g, colors[BLUE].b);
+	const bluePointTile = tilesPosition[5][points[BLUE]];
+	if( points[0] === points[1] ) {
+		arc(bluePointTile.X, bluePointTile.Y, tileWidth/2, tileWidth/2, PI, 2*PI, CHORD);
+	} else {
+		ellipse(bluePointTile.X, bluePointTile.Y, tileWidth/2);
+	}
+	fill(colors[RED].r, colors[RED].g, colors[RED].b);
+	const redPointTile = tilesPosition[5][points[RED]];
+	if( points[0] === points[1] ) {
+		arc(bluePointTile.X, bluePointTile.Y, tileWidth/2, tileWidth/2, 0, PI, CHORD);
+	} else {
+		ellipse(bluePointTile.X, bluePointTile.Y, tileWidth/2);
+	}
+	pop();
+}
+
 function drawGame() {
 	spritesheet.drawSprite("board", 0, 300, 2);
 
 	push();
-	translate(1140+240,45);
-	rotate(PI/2);
-	spritesheet.drawSprite("verso", 0, 0, 0);
-	translate(190, 0);
-	spritesheet.drawSprite("verso", 0, 0, 0);
-	translate(190, 0);
-	spritesheet.drawSprite("cards", 3, 0, 0);
-	translate(190, 0);
-	spritesheet.drawSprite("cards", 1, 0, 0);
+	noFill();
+	strokeWeight(3);
+	stroke(0);
+	// RED
+	drawCard(RED, 0);
+	drawCard(RED, 1);
+	drawCard(RED, 2);
+	drawCard(RED, 3);
+
+	// BLUE
+	drawCard(BLUE, 0);
+	drawCard(BLUE, 1);
+	drawCard(BLUE, 2);
+	drawCard(BLUE, 3);
+
+	drawPoints();
+
+	if( selectedCardIdx !== -1 ) {
+		drawCard(playerIndex, selectedCardIdx, {X: mouseX, Y: mouseY, width: 260, height: 165});
+	}
 	pop();
 
-	push();
-	translate(270,45);
-	rotate(PI/2);
-	spritesheet.drawSprite("cards", 4, 0, 0);
-	translate(190, 0);
-	spritesheet.drawSprite("cards", 5, 0, 0);
-	translate(190, 0);
-	spritesheet.drawSprite("cards", 6, 0, 0);
-	translate(190, 0);
-	spritesheet.drawSprite("cards", 7, 0, 0);
-	pop();
+	// DEBUG: tile position
+	/*
+	fill(colors[BLUE].r, colors[BLUE].g, colors[BLUE].b);
+	strokeWeight(1);
+	stroke(0);
+	tilesPosition[0].forEach(tile=>ellipse(tile.X, tile.Y, tileWidth));
+	tilesPosition[1].forEach(tile=>ellipse(tile.X, tile.Y, tileWidth));
+	tilesPosition[2].forEach(tile=>ellipse(tile.X, tile.Y, tileWidth));
+	tilesPosition[3].forEach(tile=>ellipse(tile.X, tile.Y, tileWidth));
+	tilesPosition[4].forEach(tile=>ellipse(tile.X, tile.Y, tileWidth));
+	tilesPosition[5].forEach(tile=>arc(tile.X, tile.Y, tileWidth/2, tileWidth/2, PI, 2*PI, CHORD));
+	fill(colors[RED].r, colors[RED].g, colors[RED].b);
+	tilesPosition[5].forEach(tile=>arc(tile.X, tile.Y, tileWidth/2, tileWidth/2, 0, PI, CHORD));
+	*/
 }
 
 function initGame() {
 	// shuffle cards
+	shuffleArray(cards);
+
+	// TODO: server
+	curCards[0].push({value: cards.pop(), visible: true});
+	curCards[0].push({value: cards.pop(), visible: true});
+	curCards[0].push({value: cards.pop(), visible: true});
+	curCards[0].push({value: cards.pop(), visible: true});
+	curCards[1].push({value: cards.pop(), visible: false});
+	curCards[1].push({value: cards.pop(), visible: false});
+	curCards[1].push({value: cards.pop(), visible: false});
+	curCards[1].push({value: cards.pop(), visible: false});
 }
 
 function drawLoading() {
@@ -164,7 +318,7 @@ function draw() {
 
     // draw game
 	if( curState === GAME_START_STATE ) {
-		spritesheet.drawSprite("cover", 0, 640, 190);
+		spritesheet.drawSprite("cover", 0, 90, 100);
 	}
 	if (curState === GAME_PLAY_STATE) {
 		updateGame(elapsedTime);
@@ -189,8 +343,19 @@ function draw() {
     lastTime = currentTime;
 }
 
+function emit(type, data) {
+}
+
 const between = (min, value, max) => {
 	return value >= min && value <= max;
+}
+
+const inRectangle = (topLeft, bottomRight) => {
+	return between(topLeft.X,mouseX,bottomRight.X) && between(topLeft.Y,mouseY,bottomRight.Y);
+}
+
+const mouseOverCard = (card) => {
+	return inRectangle({X: card.X, Y: card.Y},{X: card.X+card.width, Y: card.Y+card.height});
 }
 
 function cardClicked() {
@@ -227,6 +392,32 @@ function tileClicked() {
 	return null;
 }
 
+function mouseMoved() {
+	overCardIdx = -1;
+	if( selectedCardIdx === -1 ) {
+		cardsPosition[playerIndex].forEach((card,index)=>{
+			if( mouseOverCard(card) ) {
+				overCardIdx = index;
+			}
+		});
+	} else {
+		cardsPosition[playerIndex].forEach((card,index)=>{
+			if( index !== selectedCardIdx && mouseOverCard(card) ) {
+				// switch cards
+				const curCard = curCards[playerIndex][selectedCardIdx];
+				curCards[playerIndex].splice(selectedCardIdx, 1);
+				curCards[playerIndex].splice(index, 0, curCard);
+				/*
+				
+				curCards[playerIndex][selectedCardIdx] = curCards[playerIndex][index];
+				curCards[playerIndex][index] = curCard;
+				*/
+				selectedCardIdx = index;
+			}
+		});
+	}
+}
+
 function mouseClicked() {
 	if( displayRules ) {
 		displayRules = !displayRules;
@@ -237,6 +428,13 @@ function mouseClicked() {
 	}
 	if( toggleDebug ) {
 		uiManager.addLogger(`X=${mouseX}, Y=${mouseY}`);
+	}
+	if( selectedCardIdx !== -1 ) {
+		selectedCardIdx = -1;
+	}
+	if( overCardIdx !== -1 ) {
+		selectedCardIdx = overCardIdx;
+		overCardIdx = -1;
 	}
 	toolManager.mouseClicked();
 	uiManager.mouseClicked();
@@ -280,4 +478,14 @@ function keyPressed() {
 	if (key === "D") {
 		toggleDebug = !toggleDebug;
 	}
+}
+
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+const shuffleArray = (array) => {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i+1))
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
