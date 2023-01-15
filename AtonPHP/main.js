0cd7dc683ebf0331fb20f8e094bed22f40a50a2f
Stephane <stephane.conreaux@yahoo.fr>
2023-01-15 10:23:58 +0100

    AtonPHP: new game

---
+++ AtonPHP/main.js
@@ -0,0 +1,159 @@
+const uiManager = new UIManager();
+const windowWidth = 1400;
+const windowHeight = 800;
+uiManager.loggerContainer = new LoggerContainer(windowWidth-300, windowHeight-100, 240, 100);
+uiManager.loggerContainer.visible = true;
+
+const toolManager = new ToolManager();
+const jobManager = new JobManager();
+const soundManager = new SoundMgr();
+const spritesheet = new SpriteSheet();
+
+const GAME_LOADING_STATE = 0;
+const GAME_START_STATE = 1;
+const GAME_PLAY_STATE = 2;
+const GAME_OVER_STATE = 3;
+let curState = GAME_LOADING_STATE;
+let toggleDebug = false;
+let lastTime = 0;
+
+let a = 0;
+
+function preload() {
+}
+
+function musicClicked() {
+	// TODO
+}
+
+const speakerStorageKey = 'DrDr3ck/GameEngine/Speaker';
+function speakerClicked() {
+	speakerButton.checked = !speakerButton.checked;
+	soundManager.mute(!speakerButton.checked);
+	localStorage.setItem(speakerStorageKey, speakerButton.checked?"on":"off");
+}
+
+function startClicked() {
+	curState = GAME_PLAY_STATE;
+	uiManager.setUI([ speakerButton, musicButton ]);
+	uiManager.addLogger("Start game");
+}
+
+const speakerButton = new BFloatingSwitchButton(windowWidth - 70 - 10 - 70, 70, '\uD83D\uDD0A', speakerClicked);
+const musicButton = new BFloatingSwitchButton(windowWidth - 70, 70, '\uD83C\uDFB6', musicClicked);
+const startButton = new BButton(80, windowHeight - 50 - 200, "START", startClicked);
+
+function initUI() {
+    speakerButton.setTextSize(50);
+	musicButton.setTextSize(50);
+	musicButton.enabled = false;
+	musicButton.checked = false;
+	const isSpeakerOn = localStorage.getItem(speakerStorageKey);
+	if( isSpeakerOn === "off" ) {
+		speakerButton.checked = false;
+		soundManager.mute(true);
+	}
+	const menu = [ speakerButton, startButton, musicButton ];
+	uiManager.setUI(menu);
+}
+
+function setup() {
+    initUI();
+	canvas = createCanvas(windowWidth, windowHeight);
+    canvas.parent('canvas');
+
+    frameRate(60);
+
+    lastTime = Date.now();
+}
+
+function updateGame(elapsedTime) {
+
+}
+
+function drawGame() {
+	fill(0);
+	noStroke();
+	textSize(50);
+	textAlign(CENTER, CENTER);
+	text(a, 50,50);
+}
+
+function initGame() {
+
+}
+
+function drawLoading() {
+	fill(0);
+	noStroke();
+	textSize(50);
+	textAlign(CENTER, CENTER);
+	text('Loading...', width / 2, height / 2);
+	if (
+		soundManager.totalLoadedSounds === soundManager.soundToLoad &&
+		spritesheet.totalLoadedImages === spritesheet.totalImagesToLoad
+	) {
+		curState = GAME_START_STATE;
+
+        // init game
+        initGame();
+		textAlign(LEFT, BASELINE);
+		uiManager.addLogger('Game loaded');
+	}
+}
+
+function draw() {
+    const currentTime = Date.now();
+	const elapsedTime = currentTime - lastTime;
+    background(51);
+    if (curState === GAME_LOADING_STATE) {
+		drawLoading();
+		return;
+	}
+
+    uiManager.processInput();
+
+    uiManager.update(elapsedTime);
+
+    // draw game
+	if( curState === GAME_START_STATE ) {
+	}
+	if (curState === GAME_PLAY_STATE) {
+		updateGame(elapsedTime);
+		drawGame();
+	}
+
+    uiManager.draw();
+	if (toolManager.currentTool) {
+		toolManager.currentTool.draw();
+	}
+    jobManager.draw();
+    
+    lastTime = currentTime;
+}
+
+function mouseClicked() {
+	if( toggleDebug ) {
+		uiManager.addLogger(`X=${mouseX}, Y=${mouseY}`);
+	}
+	toolManager.mouseClicked();
+	uiManager.mouseClicked();
+	return false;
+}
+
+function askPHP(a1,a2) {
+	var xmlHttp = new XMLHttpRequest();
+  	xmlHttp.open( "GET", "./aton_server.php?a="+a1+"&b="+a2, false );
+  	xmlHttp.send( null );
+  	return xmlHttp.responseText; 
+}
+
+function keyPressed() {
+	if (key === "D") {
+		toggleDebug = !toggleDebug;
+	}
+
+	if( key === "A") {
+		a = askPHP(3,4);
+	}
+}
